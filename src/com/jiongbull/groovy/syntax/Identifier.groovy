8e763e19b6b6b4b2cbafc55989e192ee7bb6a8c1
JiongBull <jiongbull717@gmail.com>
2015-12-10 14:19:23 +0800

    测试定义map

--- src/com/jiongbull/groovy/syntax/Identifier.groovy
+++ src/com/jiongbull/groovy/syntax/Identifier.groovy
@@ -5,13 +5,16 @@ package com.jiongbull.groovy.syntax
  */
 /* --------------------普通标识符-------------------- */
 /* 正确的定义. */
-def name;
-def $name;
-def name_type;
+def name
+def $name
+def name_type
 
 /* 错误的定义 */
-//def foo.assert;
-//def 5type;
-//def a+b;
+//def foo.assert
+//def 5type
+//def a+b
 
 /* --------------------引用标识符-------------------- */
+def map = [:]
+map."an identifier with a space and double quotes" = "ALLOWED"
+map.'with-dash-signs-and-single-quotes' = "ALLOWED"
