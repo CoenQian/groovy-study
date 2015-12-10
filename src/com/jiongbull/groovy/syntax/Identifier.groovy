b015da6a1c7aeff74f0d8ba3f455b002fae7c1bc
JiongBull <jiongbull717@gmail.com>
2015-12-10 13:59:15 +0800

    创建项目

---
+++ src/com/jiongbull/groovy/syntax/Identifier.groovy
@@ -0,0 +1,17 @@
+package com.jiongbull.groovy.syntax
+
+/**
+ * 标识符.
+ */
+/* --------------------普通标识符-------------------- */
+/* 正确的定义. */
+def name;
+def $name;
+def name_type;
+
+/* 错误的定义 */
+//def foo.assert;
+//def 5type;
+//def a+b;
+
+/* --------------------引用标识符-------------------- */
